81f5201779b00789d17c0f3f275ff129dc527d26
Levent Ozkul <you@example.comleventozkul2021@gmail.com>
2022-10-04 18:41:58 +0200

    C06

--- src/main/java/day02_driverMethods/C02_IlkTest.java
+++ src/main/java/day02_driverMethods/C02_IlkTest.java
@@ -1,37 +1,40 @@
 package day02_driverMethods;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class C02_IlkTest {
     public static void main(String[] args) {
-
-        System.setProperty("webdriver.chrome.driver", "src/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.amazon.se");
-
+        /*
+          Gerekli ayarlamalari yapip amazon anasayfaya gidin
+          1- sayfa basligi "amazon" kelimesi icermeli
+          2- sayfa url'inde "Amazon" kelimesi olmali
+         */
+        System.setProperty("webdriver.chrome.driver","src/resources/chromedriver");
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://www.amazon.com");
         String expectedTitleIcerik="amazon";
-        String expectedURLIcerik="Amazon";
-
-        String actualURL= driver.getCurrentUrl();
-        String actualTitle = driver.getTitle();
-
-        if (actualURL.contains(expectedURLIcerik)){
-            System.out.println("URL test passed");
-        } else {
-            System.out.println("URL test failed");
-            System.out.println("actual URL : " + actualURL);
-            System.out.println("Actual URL aranan " + expectedURLIcerik + " kelimesini içermiyor");
+        String expectedUrlIcerik="Amazon";
+        String actualUrl= driver.getCurrentUrl();
+        String actualTitle= driver.getTitle();
+        // url test yapalim
+        if (actualUrl.contains(expectedUrlIcerik)){
+            System.out.println("Url test PASSED");
+        }else {
+            System.out.println("Url test FAILED");
+            System.out.println("actual Url : " + actualUrl);
+            System.out.println("Actual Url aranan " + expectedUrlIcerik + " kelimesini icermiyor");
         }
-
+        // title test
         if (actualTitle.contains(expectedTitleIcerik)){
-            System.out.println("Title test passed");
-        } else {
-            System.out.println("Title test failed");
-            System.out.println("actual Title : " + actualTitle);
-            System.out.println("Actual Title aranan " + expectedTitleIcerik + " kelimesini içermiyor");
+            System.out.println("Title Testi PASSED");
+        }else {
+            System.out.println("Title testi FAILED");
+            System.out.println("Actual Title : " + actualTitle);
+            System.out.println("Actual title aranan " + expectedTitleIcerik + " kelimesini icermiyor");
         }
         driver.quit();
-
+        /*
+        driver.close sadece acik olan browser'i kapatir
+        driver.quit ise test calisirken acilan tum browser'lari kapatir
+         */
     }
 }
