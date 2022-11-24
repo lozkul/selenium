6cd24d69e8531d374e803d32768a926bc72bf22e
Levent Ozkul <you@example.comleventozkul2021@gmail.com>
2022-11-24 02:55:56 +0100

    24.11

--- src/test/java/Practise/Q3.java
+++ src/test/java/Practise/Q3.java
@@ -1,5 +1,13 @@
 package Practise;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class Q3 {
     public static void main(String[] args) {
 
@@ -17,8 +25,29 @@ public class Q3 {
     click submit button
  */
 
+        System.setProperty("webdriver.chrome.driver", "src/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://www.techlistic.com/p/selenium-practice-form.html");
+        driver.manage().deleteAllCookies();
 
-
-
+        driver.findElement(By.xpath("//button[@id='ez-accept-all']")).click();
+        driver.findElement(By.xpath("//a[@id='cookieChoiceDismiss']")).click();
+        driver.findElement(By.xpath("//input[@name='firstname']")).sendKeys("Levent" + Keys.ENTER);
+        driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys("Özkul" + Keys.ENTER);
+        WebElement genderRadioButton =driver.findElement(By.xpath("//input[@id='sex-0']"));
+        genderRadioButton.click();
+        genderRadioButton.sendKeys(Keys.PAGE_DOWN);
+        driver.findElement(By.xpath("//input[@id='exp-6']")).click();
+        driver.findElement(By.xpath("//input[@id='datepicker']")).sendKeys("11.10.2022" + Keys.ENTER);
+        driver.findElement(By.xpath("//input[@id='profession-1']")).click();
+        driver.findElement(By.xpath("//input[@id='tool-2']")).click();
+       // driver.findElement(By.xpath("(//option[@style='margin: 0px; padding-bottom: 0px; padding-left: 0px; padding-right: 0px;'])[7]")).click();
+        driver.findElement(By.xpath("//select[@class='input-xlarge']")).sendKeys("Antartica"+ Keys.ENTER+ Keys.ENTER);
+       // driver.findElement(By.xpath("(//option[@style='margin: 0px; padding-bottom: 0px; padding-left: 0px; padding-right: 0px;'])[8]")).click();
+        driver.findElement(By.xpath("//select[@id='selenium_commands']")).sendKeys("Browser Commands"+ Keys.ENTER);
+        driver.findElement(By.xpath("//button[@class='btn btn-info']")).click();
+        driver.close();
     }
 }
